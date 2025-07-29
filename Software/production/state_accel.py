f0c52554342bf055eff4f7b3b6a0c1d4e7459154
Jason Andrews <lithochasm@Mac.home.arpa>
2025-07-28 18:26:08 -0700

    Vibe coded some acclerometer stuff

--- Software/production/state_accel.py
+++ Software/production/state_accel.py
@@ -2,6 +2,7 @@ from setup import pixels, num_pixels
 from setup import keys
 from setup import sensor
 from state import State
+from rainbowio import colorwheel
 #from menu import menu_select, show_menu, show_select
 
 
@@ -11,9 +12,13 @@ class AccelState(State):
     def name(self):
         return "accel"
 
+    def __init__(self):
+        self.hue = 0
+        self.target_hue = 0
+
     def enter(self, machine):
         pixels.fill((0,0,0))
-        
+        pixels.brightness = 0.1
         State.enter(self, machine)
 
     def exit(self, machine):
@@ -30,22 +35,35 @@ class AccelState(State):
         # Get accelerometer data
         acc_x, acc_y, acc_z = sensor.acceleration
 
-        # Light leds based on tilt
-        pixels.fill((0,0,0))
-        if abs(acc_x) > abs (acc_y):
-            if acc_x > 0:
-                for led in range(35,42):
-                    pixels[led] = (0,0,255)
-            else:
-                for led in range(0,7):
-                    pixels[led] = (0,0,255)
-
+        # Set target hue based on tilt direction
+        if abs(acc_x) > abs(acc_y):
+            if acc_x > 0:  # Tilted right
+                self.target_hue = 0  # Red
+            else:  # Tilted left
+                self.target_hue = 85  # Green
         else:
-            if acc_y > 0:
-                for led in range(0,6):
-                    pixels[led*7] = (0,0,255)
+            if acc_y > 0:  # Tilted forward
+                self.target_hue = 170  # Blue
+            else:  # Tilted back
+                self.target_hue = 255  # Purple
+
+        # Smooth fade towards target hue
+        if self.hue != self.target_hue:
+            if self.hue < self.target_hue:
+                if self.target_hue - self.hue > 128:
+                    self.hue = (self.hue - 1) % 256
+                else:
+                    self.hue = (self.hue + 1) % 256
             else:
-               for led in range(0,6):
-                    pixels[led*7+6] = (0,0,255)
+                if self.hue - self.target_hue > 128:
+                    self.hue = (self.hue + 1) % 256
+                else:
+                    self.hue = (self.hue - 1) % 256
+
+        # Get the current color
+        color = colorwheel(self.hue)
+
+        # Fill the entire LED grid with the color
+        pixels.fill(color)
         pixels.show()
  
